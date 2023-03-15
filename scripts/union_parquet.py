322f7249ad0904ebdd75a9bb9d44efda6b677925
Krzysztof P <krzypluc@gmail.com>
2023-03-15 18:00:25 +0100

    update

--- scripts/union_parquet.py
+++ scripts/union_parquet.py
@@ -3,5 +3,9 @@ import pandas as pd
 if __name__=="__main__":
     parquet_directory = "./data/clean_topo"
     df = pd.read_parquet(parquet_directory)
+    df = df.loc[df["rodzaj"] == "Twt"]
     
-    print(df.shape)
+    df_2 = pd.read_csv("./data/clean_res_list.csv")
+    df_2 = df_2.loc[df_2["rodzaj"] == "WIL"]
+    
+    print(df_2)
