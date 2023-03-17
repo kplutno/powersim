036536f28a038801c246085c3768c832820823b5
Krzysztof P <krzypluc@gmail.com>
2023-03-17 14:31:42 +0100

    add more of text

--- agent_based/run.py
+++ agent_based/run.py
@@ -1,17 +1,20 @@
 from agent_based.models.base_model import ModelV1
 from utils.process import process_ure_rse_source, load_config, load_clean_res_data
+from utils.topo_data_process import process_topo
+
 
 if __name__ == "__main__":
     config_file = "./config/config.yml"
     config = load_config(config_file)
 
-    try:
-        wind_sources, pv_sources, res = load_clean_res_data(config)
-
-    except FileNotFoundError:
+    # Data processing before 
+    if config.process.process_ure_rse_source:
         process_ure_rse_source(config)
-        wind_sources, pv_sources, res = load_clean_res_data(config)
 
+    if config.process.process_topo:
+        process_topo(config)
+    
+    
     model = ModelV1(wind_sources, pv_sources, time_list=[0, 1, 2])
 
     for i in range(2):
--- scripts/get_coordinate.py
+++
@@ -1,5 +0,0 @@
-import os
-
-if __name__ == "__main__":
-    api_key = os.environ["METEO_API_KEY"]
-    headers = {"Authorization": "Token {}".format(api_key)}
